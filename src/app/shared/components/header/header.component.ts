6e9ea3d55b1a25e7b1e39bcbec837e6802a86d05
zlemisie <zlemisie@gmail.com>
2017-06-09 23:47:55 +0200

    firebase login disabled

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService} from '@ngx-translate/core';
 import { Router } from '@angular/router';
-import { AngularFire } from 'angularfire2';
 
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/first';
@@ -14,7 +13,7 @@ import 'rxjs/add/operator/first';
 })
 export class HeaderComponent implements OnInit {
 
-    constructor(public af: AngularFire, private translate: TranslateService, private router: Router) { }
+    constructor(private translate: TranslateService, private router: Router) { }
 
     ngOnInit() {}
 
@@ -30,15 +29,6 @@ export class HeaderComponent implements OnInit {
 
     onLoggedout() {
         localStorage.removeItem('isLoggedin');
-        this.af.auth
-        // You only want unathenticated states:
-            .filter((authState) => !authState)
-            // You only want the first unathenticated state:
-            .first()
-            // You should now be able to navigate:
-            .subscribe(() => this.router.navigate(['/login']));
-        // The composed observable completes, so there's no need to unsubscribe.
-        this.af.auth.logout();
     }
 
     changeLang(language: string) {
